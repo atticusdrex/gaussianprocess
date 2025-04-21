0a7af4aa45d9d4e3d5a0b85832941fd66b80a89d
atticusdrex <atticusdrex@gmail.com>
2025-04-21 12:33:31 -0400

    Updating init

--- gaussianprocess/__init__.py
+++ gaussianprocess/__init__.py
@@ -1,3 +1,7 @@
 # gausisanprocess/__init__.py
 
-from .gaussianprocess import *  # <-- add any other useful classes/functions
+from .gaussianprocess import rbf_kernel, laplace_kernel, K, GaussianProcess 
+
+__all__ = [
+    "rbf_kernel", "laplace_kernel", "K", "GaussianProcess" 
+]
